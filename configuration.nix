4b63a99ef0be02ae45dc33eb28efe03e07d6b75c
Makar Haustein <makarmaxha@gmail.com>
2024-09-29 22:59:45 +0200

    Configured nvim to be default editor

--- configuration.nix
+++ configuration.nix
@@ -66,14 +66,19 @@
   # List packages installed in system profile. To search, run: $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. wget
+    zsh
     git 
     gh 
     neovim 
     libgcc
   ];
-
-  programs.neovim.defaultEditor = true;
-
+  
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
+  };
   # Some programs need SUID wrappers, can be configured further or are started in user sessions. programs.mtr.enable = true; programs.gnupg.agent = {
   #   enable = true; enableSSHSupport = true;
   # };
